19d08c51bffe22bca956dffe7d8faaa349978e85
Kyle Chinick <kylechinick@icloud.com>
2022-06-01 16:16:33 -0700

    Add function to toggle light and dark divs
    Co-authored-by: Jonathan Lu <jdlu.coding@gmail.com>
    Co-authored-by: Ashe Urban <ashe@goldentongue.com>
    dquote> Co-authored-by: JaredGran <jared.carper@gmail.com>

--- js/scripts.js
+++ js/scripts.js
@@ -4,26 +4,41 @@
 //   })
 // })
 
-$(document).ready(function () {
-  $('.toggle-theme').click(function () {
-    $("body").removeClass();
-    $("body").addClass("dark-color")
-  });
-});
+// $(document).ready(function () {
+//   $('.toggle-theme').click(function () {
+//     $("body").removeClass();
+//     $("body").addClass("dark-color")
+//   });
+// });
 
-$(document).ready(function () {
-  $('.toggle-theme').click(function () {
-    $("body").removeClass();
-    $("body").addClass("light-color")
-  });
-});
+// $(document).ready(function () {
+//   $('.toggle-theme').click(function () {
+//     $("body").removeClass();
+//     $("body").addClass("light-color")
+//   });
+// });
 
-$(document).ready(function () {
-  $(".clickable").click(function() {
-    $("#hide-button").toggle();
-    $("#show-button").toggle();
-  });
-});
+// $(document).ready(function () {
+//   $(".clickable").click(function() {
+//     $("#hide-button").toggle();
+//     $("#show-button").toggle();
+//   });
+// });
 
-// 
+//
 // button/ toggles to dark/ remove dark/
+
+$(document).ready(function () {
+  $('.clickable').click(function () {
+    $('#dark-div').toggle();
+    $('#light-div').toggle();
+  });
+});
+
+// User lands on page with light background
+
+// User clicks theme button
+// -- Div background changed to dark color
+
+// User clicks theme button AGAIN
+// -- Div background toggles 
