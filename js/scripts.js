647c672a1f41c7371ee023d0088be699bcf09f69
Kyle Chinick <kylechinick@icloud.com>
2022-06-01 15:52:45 -0700

    WIP Dark theme
    Co-authored-by: Jonathan Lu <jdlu.coding@gmail.com>
    Co-authored-by: Ashe Urban <ashe@goldentongue.com>
    dquote> Co-authored-by: JaredGran <jared.carper@gmail.com>

--- js/scripts.js
+++ js/scripts.js
@@ -5,7 +5,25 @@
 // })
 
 $(document).ready(function () {
-  $('.darkThemeBtn').click(function () {
-    $('body').addClass('dark-color');
+  $('.toggle-theme').click(function () {
+    $("body").removeClass();
+    $("body").addClass("dark-color")
   });
 });
+
+$(document).ready(function () {
+  $('.toggle-theme').click(function () {
+    $("body").removeClass();
+    $("body").addClass("light-color")
+  });
+});
+
+$(document).ready(function () {
+  $(".clickable").click(function() {
+    $("#hide-button").toggle();
+    $("#show-button").toggle();
+  });
+});
+
+// 
+// button/ toggles to dark/ remove dark/
