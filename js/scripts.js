c66a40588723fd0bc48495616f7e5ed745399f6b
Kyle Chinick <kylechinick@icloud.com>
2022-06-01 15:19:17 -0700

    Add dark theme
    Co-authored-by: Jonathan Lu <jdlu.coding@gmail.com>
    Co-authored-by: Ashe Urban <ashe@goldentongue.com>
    Co-authored-by: JaredGran <jared.carper@gmail.com>

---
+++ js/scripts.js
@@ -0,0 +1,11 @@
+// $(document).ready(function() {
+//   $("darkThemeBtn").click(function() {
+//     $("body").addClass(".chartreuse-background");
+//   })
+// })
+
+$(document).ready(function () {
+  $('.darkThemeBtn').click(function () {
+    $('body').addClass('dark-color');
+  });
+});
