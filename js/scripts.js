d8afea248a9e7da2cc0d8eb82971ae3f03a0069d
Kyle Chinick <kylechinick@icloud.com>
2022-06-01 17:56:14 -0700

    Extra commit to confirm work is pushing to remote repo

--- js/scripts.js
+++ js/scripts.js
@@ -1,44 +1,6 @@
-// $(document).ready(function() {
-//   $("darkThemeBtn").click(function() {
-//     $("body").addClass(".chartreuse-background");
-//   })
-// })
-
-// $(document).ready(function () {
-//   $('.toggle-theme').click(function () {
-//     $("body").removeClass();
-//     $("body").addClass("dark-color")
-//   });
-// });
-
-// $(document).ready(function () {
-//   $('.toggle-theme').click(function () {
-//     $("body").removeClass();
-//     $("body").addClass("light-color")
-//   });
-// });
-
-// $(document).ready(function () {
-//   $(".clickable").click(function() {
-//     $("#hide-button").toggle();
-//     $("#show-button").toggle();
-//   });
-// });
-
-//
-// button/ toggles to dark/ remove dark/
-
 $(document).ready(function () {
   $('.clickable').click(function () {
     $('.dark-div').toggle();
     $('.light-div').toggle();
   });
 });
-
-// User lands on page with light background
-
-// User clicks theme button
-// -- Div background changed to dark color
-
-// User clicks theme button AGAIN
-// -- Div background toggles 
