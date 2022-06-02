a757278b262b87549e2b5a17a1d7ae2e911a5724
Kyle Chinick <kylechinick@icloud.com>
2022-06-01 18:23:00 -0700

    Add 1 function to activate dark mode and another function to activate light mode

---
+++ js/scripts.js
@@ -0,0 +1,16 @@
+$(document).ready(function () {
+  $('.button_dark-theme').click(function () {
+    $('body').removeClass();
+    $('body').addClass('dark-background');
+    $('.button_dark-theme').toggle();
+    $('.button_light-theme').toggle();
+  });
+});
+
+$(document).ready(function () {
+  $('.button_light-theme').click(function () {
+    $('body').removeClass();
+    $('.button_light-theme').toggle();
+    $('.button_dark-theme').toggle();
+  });
+});
